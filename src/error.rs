786919dc26b1309ad595432aeda5bfba0a313b1d
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2024-12-08 17:25:59 +0700

    chore!: migrate MangaError and AuthError into ApiError

--- src/error.rs
+++ src/error.rs
@@ -2,7 +2,7 @@ use axum::http::StatusCode;
 use axum_thiserror::ErrorStatus;
 
 #[derive(thiserror::Error, ErrorStatus)]
-pub enum MangaError {
+pub enum ApiError {
     #[error("Manga is missing")]
     #[status(StatusCode::NOT_FOUND)]
     Missing(#[source] anyhow::Error),
@@ -15,49 +15,44 @@ pub enum MangaError {
     #[status(StatusCode::UNAUTHORIZED)]
     InvalidCredential(#[source] anyhow::Error),
 
+    #[error("Invalid Credential")]
+    #[status(StatusCode::UNAUTHORIZED)]
+    InvalidAuthToken(#[source] anyhow::Error),
+
     #[error("Content Equal")]
     #[status(StatusCode::NO_CONTENT)]
     ContentEqual(#[source] anyhow::Error),
-}
 
-#[derive(thiserror::Error, ErrorStatus)]
-pub enum AuthError {
     #[error("User is missing")]
     #[status(StatusCode::BAD_REQUEST)]
     UserMissing(#[source] anyhow::Error),
+
     #[error("Incorrect email format")]
     #[status(StatusCode::BAD_REQUEST)]
     ValidationEmailInvalid(#[source] anyhow::Error),
+
     #[error("Email length must be between 3 and 128 characters")]
     #[status(StatusCode::BAD_REQUEST)]
     ValidationEmailLength(#[source] anyhow::Error),
+
     #[error("Password length must be between 8 and 128 characters")]
     #[status(StatusCode::BAD_REQUEST)]
     ValidationPasswordLength(#[source] anyhow::Error),
+
     #[error("Invalid credential")]
     #[status(StatusCode::BAD_REQUEST)]
     InvalidPassword(#[source] anyhow::Error),
-    #[error("Something went wrong")]
-    #[status(StatusCode::INTERNAL_SERVER_ERROR)]
-    UnexpectedError(#[from] anyhow::Error),
+
     #[error("Auth header is missing")]
     #[status(StatusCode::UNAUTHORIZED)]
     EmptyAuthHeader(#[source] anyhow::Error),
+
     #[error("Auth token is missing")]
     #[status(StatusCode::UNAUTHORIZED)]
     EmptyAuthToken(#[source] anyhow::Error),
-    #[error("Invalid Credential")]
-    #[status(StatusCode::UNAUTHORIZED)]
-    InvalidCredential(#[source] anyhow::Error),
 }
 
-impl std::fmt::Debug for MangaError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        error_chain_fmt(self, f)
-    }
-}
-
-impl std::fmt::Debug for AuthError {
+impl std::fmt::Debug for ApiError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         error_chain_fmt(self, f)
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod authorization;
 pub mod configuration;
+pub mod error;
+pub mod model;
 pub mod router;
 pub mod startup;
 pub mod telemetry;
-pub mod util;
