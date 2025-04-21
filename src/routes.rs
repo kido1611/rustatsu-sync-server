3d3da4919bd43cb901bacded6920411de7656601
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-04-21 21:30:06 +0700

    feat: add request-id as header response and enable tracing access log

--- src/routes.rs
+++ src/routes.rs
@@ -2,13 +2,22 @@ use std::sync::Arc;
 
 use axum::{
     Router,
-    extract::DefaultBodyLimit,
+    body::Body,
+    extract::{DefaultBodyLimit, MatchedPath},
+    http::{HeaderName, Request, header},
     middleware,
     routing::{get, post},
 };
-use tower_http::compression::CompressionLayer;
+use tower::ServiceBuilder;
+use tower_http::{
+    compression::CompressionLayer,
+    request_id::{MakeRequestUuid, PropagateRequestIdLayer, SetRequestIdLayer},
+    trace::TraceLayer,
+};
 
 use crate::{middlewares::jwt_auth_middleware, state::AppState};
+
+const REQUEST_ID_HEADER: &str = "x-request-id";
 
 pub fn init_router(app_state: AppState) -> Router {
     let state = Arc::new(app_state);
@@ -46,32 +55,46 @@ pub fn init_router(app_state: AppState) -> Router {
             jwt_auth_middleware,
         ));
 
+    let x_request_id_header = HeaderName::from_static(REQUEST_ID_HEADER);
+    let request_id_middleware = ServiceBuilder::new()
+        .layer(SetRequestIdLayer::new(
+            x_request_id_header.clone(),
+            MakeRequestUuid,
+        ))
+        .layer(
+            TraceLayer::new_for_http().make_span_with(|request: &Request<Body>| {
+                let request_id = match request.headers().get(REQUEST_ID_HEADER) {
+                    Some(val) => val.to_str().unwrap(),
+                    None => "",
+                };
+                let user_agent = match request.headers().get(header::USER_AGENT) {
+                    Some(val) => val.to_str().unwrap(),
+                    None => "",
+                };
+
+                let matched_path = request
+                    .extensions()
+                    .get::<MatchedPath>()
+                    .map(MatchedPath::as_str);
+
+                tracing::info_span!(
+                    "http_request",
+                    request_id,
+                    method = ?request.method(),
+                    uri = ?request.uri(),
+                    path = matched_path,
+                    version = ?request.version(),
+                    user_agent,
+                )
+            }),
+        )
+        .layer(PropagateRequestIdLayer::new(x_request_id_header));
+
     app.nest("/manga", manga_route)
         .nest("/me", me_route)
         .nest("/resource/favourites", resources_favourites_route)
         .nest("/resource/history", resources_history_route)
-        .with_state(state)
         .layer(CompressionLayer::new())
-    // .layer(
-    //     TraceLayer::new_for_http()
-    //         .make_span_with(
-    //             |request: &Request<Body>| {
-    //                 let request_id = uuid::Uuid::new_v4();
-    //                 tracing::span!(
-    //                     Level::INFO,
-    //                     "request",
-    //                     method = tracing::field::display(request.method()),
-    //                     uri = tracing::field::display(request.uri()),
-    //                     version = tracing::field::debug(request.version()),
-    //                     request_id = tracing::field::display(request_id),
-    //                     headers = tracing::field::debug(request.headers())
-    //                 )
-    //             }, // trace::DefaultMakeSpan::new()
-    //                //     .level(Level::INFO)
-    //                //     .include_headers(true),
-    //         )
-    //         .on_request(trace::DefaultOnRequest::new().level(Level::INFO))
-    //         .on_response(trace::DefaultOnResponse::new().level(Level::INFO))
-    //         .on_failure(DefaultOnFailure::new().level(Level::ERROR)),
-    // )
+        .layer(request_id_middleware)
+        .with_state(state)
 }
