1662d4a2c3028640d6e76e01594f1403d72fa971
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2025-09-22 11:31:11 +0700

    chore(migrations): style some sql migrations

--- migrations/20250907102304_drop_is_nsfw_from_mangas_table.down.sql
+++ migrations/20250907102304_drop_is_nsfw_from_mangas_table.down.sql
@@ -1,8 +1,12 @@
 -- Add down migration script here
-ALTER TABLE mangas
-ADD COLUMN is_nsfw BOOLEAN NOT NULL
-DEFAULT false;
+ALTER TABLE
+    mangas
+ADD
+    COLUMN is_nsfw BOOLEAN NOT NULL DEFAULT false;
 
-UPDATE mangas
-SET is_nsfw = true
-WHERE content_rating = 'ADULT';
+UPDATE
+    mangas
+SET
+    is_nsfw = TRUE
+WHERE
+    content_rating = 'ADULT';
