221bd81bcd96c0a7ac2570287d322679d21d8732
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2026-01-04 20:11:38 +0700

    Revert "chore(migrations): style some sql migrations "
    This reverts commit 1662d4a2c3028640d6e76e01594f1403d72fa971.

--- migrations/20250228222432_create_favourites_table.up.sql
+++ migrations/20250228222432_create_favourites_table.up.sql
@@ -1,15 +1,23 @@
 -- Add up migration script here
 CREATE TABLE favourites (
-    manga_id bigint NOT NULL,
-    category_id bigint NOT NULL,
-    user_id bigint NOT NULL,
-    sort_key int NOT NULL,
-    created_at bigint NOT NULL,
-    deleted_at bigint NOT NULL,
+    manga_id        bigint      NOT NULL,
+    category_id     bigint      NOT NULL,
+    user_id         bigint      NOT NULL,
+    sort_key        int         NOT NULL,
+    created_at      bigint      NOT NULL,
+    deleted_at      bigint      NOT NULL,
+
     PRIMARY KEY (manga_id, category_id, user_id),
-    CONSTRAINT favourites_category_id_user_id_foreign FOREIGN KEY (category_id, user_id) REFERENCES categories (id, user_id),
-    CONSTRAINT favourites_manga_id_foreign FOREIGN KEY (manga_id) REFERENCES mangas (id),
-    CONSTRAINT favourites_user_id_foreign FOREIGN KEY (user_id) REFERENCES users (id)
+
+    CONSTRAINT favourites_category_id_user_id_foreign
+        FOREIGN KEY (category_id, user_id) REFERENCES categories (id, user_id),
+
+    CONSTRAINT favourites_manga_id_foreign
+        FOREIGN KEY (manga_id) REFERENCES mangas (id),
+
+    CONSTRAINT favourites_user_id_foreign
+        FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-CREATE INDEX favourites_user_id_index ON favourites (user_id);
+CREATE INDEX favourites_user_id_index
+    ON favourites (user_id);
--- migrations/20250907102304_drop_is_nsfw_from_mangas_table.up.sql
+++ migrations/20250907102304_drop_is_nsfw_from_mangas_table.up.sql
@@ -1,3 +1,3 @@
 -- Add up migration script here
-ALTER TABLE
-    mangas DROP COLUMN is_nsfw;
+ALTER TABLE mangas
+DROP COLUMN is_nsfw;
