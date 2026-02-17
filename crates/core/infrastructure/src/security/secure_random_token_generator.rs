5cb5cbc1288366239bced20317a6dd1af4a40b94
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2026-02-17 12:40:42 +0700

    deps: update deps

--- crates/core/infrastructure/src/security/secure_random_token_generator.rs
+++ crates/core/infrastructure/src/security/secure_random_token_generator.rs
@@ -1,6 +1,6 @@
 use base64::{Engine, engine::general_purpose::URL_SAFE_NO_PAD};
 use core_domain::security::token_generator::TokenGenerator;
-use rand::{RngCore, rng};
+use rand::{Rng, rng};
 use tracing::{Level, instrument};
 
 pub struct SecureRandomTokenGenerator {}
